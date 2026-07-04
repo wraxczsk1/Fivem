d05200ab18508f9390a06bc7bcd275f4c2584651
Baitters <kudelka.zdenek0@gmail.com>
2026-07-04 23:14:11 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'vehicles.meta',
+    'carcols.meta',
+    'carvariations.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'explosion.meta',
+    'weaponarchetypes.meta',
+    'vehicleweapons_leo2a7v.meta',
+    'dlctext.meta',
+    'caraddoncontentunlocks.meta',
+    'stream/*.yft',
+    'stream/*.ydr',
+    'stream/*.ytd'
+}
+
+data_file 'VEHICLE_METADATA_FILE'      'vehicles.meta'
+data_file 'CARCOLS_FILE'               'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'     'carvariations.meta'
+data_file 'HANDLING_FILE'              'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'       'vehiclelayouts.meta'
+data_file 'EXPLOSION_FILE'             'explosion.meta'
+data_file 'WEAPON_METADATA_FILE'       'weaponarchetypes.meta'
+data_file 'VEHICLE_WEAPON_FILE'        'vehicleweapons_leo2a7v.meta'
+data_file 'DLC_TEXT_FILE'              'dlctext.meta'
+data_file 'UNLOCKS_FILE'               'caraddoncontentunlocks.meta'
